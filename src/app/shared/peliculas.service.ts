0089a4308745153bfbf8123ac319d38d7847e545
Luis Alvarez <lealvarezp@unac.edu.co>
2020-10-19 12:48:55 -0500

    mejoras visuales pelicula

--- src/app/shared/peliculas.service.ts
+++ src/app/shared/peliculas.service.ts
@@ -7,7 +7,10 @@ import { Observable } from 'rxjs';
 })
 export class PeliculasService {
   public API = 'https://swapi.dev/api/';
-  public FILMS_API = this.API + 'films/';
+  public FILMS_API = this.API + 'films';
+  public PLANETS_API = this.API + 'planets';
+  public STARSHIPS_API = this.API + 'starships';
+  public SPECIES_API = this.API + 'species';
 
   constructor(private http: HttpClient) {
     console.log('PELICULAS: servicio listo');
@@ -18,6 +21,16 @@ export class PeliculasService {
   }
 
   getFilm(id: string) {
-    return this.http.get(this.FILMS_API + id + '/');
+    return this.http.get(this.FILMS_API + '/' + id + '/');
+  }
+
+  getPlanet(id: string) {
+    return this.http.get(this.PLANETS_API + '/' + id + '/');
+  }
+  getStarship(id: string) {
+    return this.http.get(this.STARSHIPS_API + '/' + id + '/');
+  }
+  getSpecie(id: string) {
+    return this.http.get(this.SPECIES_API + '/' + id + '/');
   }
 }
